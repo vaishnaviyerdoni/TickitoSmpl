0e8e9880db5a12105de23ddb8ba36b9fd0a7c17c
divya292003 <pandedivya580@gmail.com>
2026-06-10 18:25:31 +0530

    Package structure

---
+++ Tikito/src/main/java/com/sunbeam/tikito/TikitoApplication.java
@@ -0,0 +1,13 @@
+package com.sunbeam.tikito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikitoApplication.class, args);
+	}
+
+}
