9c173acf4493842a6aecf173d8b11fba108b3380
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2026-06-16 23:35:09 +0530

    enums, daos and modifictions in entities

---
+++ Tikito/src/main/java/com/sunbeam/tikito/daos/BookedSeatsDao.java
@@ -0,0 +1,20 @@
+package com.sunbeam.tikito.daos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sunbeam.tikito.entity.BookedSeatsEntity;
+
+public interface BookedSeatsDao extends JpaRepository<Integer, BookedSeatsEntity>
+{
+	List<BookedSeatsEntity> findByBookingBookingId(int bookingId);
+
+	void deleteByBookingBookingId(int bookingId);
+
+	boolean existsByShowShowIdAndSeatSeatId(int showId, int SeatId);
+
+	List<BookedSeatsEntity> findByShowShowIdAndSeatSeatIdIn(int showId, int seatId);
+
+	int findByShowShowId(int showId);
+}
